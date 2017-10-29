60765d4fb3fe2cfe6d3de076f304b5a55a04b915
gcgq <gary.quiso@gmail.com>
2017-10-29 18:48:55 -0500

    methods for parsing & normalizing name input added

--- app/number_cruncher.py
+++ app/number_cruncher.py
@@ -1,15 +1,41 @@
+master_numbers = [11,22,33]
 def parse_name(input=""):
     total_count, vowel_count, consonant_count = {}, {}, {}
-    reduced_numbers
 
     name_chars = list(input.lower().strip(" \s\t\r"))
     #print(name)
 
-def life_path():
+def reduce(num, reduce_master_numbers=True):
+    result = 0
+    while(num > 0):
+        result += num % 10
+        num //= 10
+        if(reduce_master_numbers is False and result in master_numbers):
+            return result
+        elif(result > 10):
+            return result
+        else:
+            return reduce(result, reduce_master_numbers)
+
+def life_path(date, reduce_master_numbers, method=0):
+    #date is the PersonData.dob of a given person.
+    #date is a datetime object and can be manipulated as such
+    result = 0
+    rmn = reduce_master_numbers
+    if method is 0:
+        result = reduce(reduce(date.year, rmn)+reduce(date.month, rmn)+reduce(date.day, rmn))
+    else:
+        result = reduce((date.year + date.month + date.day), rmn)
+    return result
+
+def expression():
     pass
 def destiny():
     pass
+    # result = {raw_total=0,}
 def hearts_desire():
+    pass
+def souls_urge():
     pass
 def personality():
     pass
---
+++ app/person_data.py
@@ -0,0 +1,52 @@
+import string, unicodedata, re
+from dateutil.parser import parse as parse_dob
+import letter
+import number_cruncher as nc
+class PersonData(object):
+    def __init__(self, name, dob):
+        # PersonData(name, dob) gets input from Flask response object, which
+        # gets data from from the front-end AJAX GET method
+        # name should be a string,
+        # dob should be a javascript Date().toUTCString() or Date().toISOString()
+        self.numbers = {}
+        self.raw_input = { 'name': name, 'dob': dob, 'ignored_characters':[] }
+        self.name = self.normalize_name(self.raw_input['name'])
+        self.date_of_birth = parse_dob(dob)
+    def normalize_name(self, name):
+        result = name
+        for char in name:
+            print(char)
+            #clean input, but preserve capitalization and some formatting for display.
+            #Letters, apostrophes, hyphens, and spaces get a pass,
+            #as they are used in names. Periods and commas are removed.
+            #Diacriticals are normalized as ascii characters.
+            #exclamation points are used to indicate a switch between consonant
+            #and vowel i.e., when 'y' makes a vowel sound
+            if char in ('!',"'",'-', ' ') or char in string.ascii_letters:
+                pass
+            else:
+                normalized_char = unicodedata.normalize('NFKD',char) \
+                                    .encode('ASCII','ignore') \
+                                    .decode('ASCII')
+                # print("char: {}, normalized: {}".format(char, normalized_char))
+                if len(normalized_char) is not 0 and normalized_char in string.ascii_letters:
+                    result = result.replace(char, normalized_char)
+                else:
+                    self.raw_input['ignored_characters'].append(char)
+                    result = result.replace(char, "")
+                    result = re.sub('\s+', ' ', result).strip()
+        # print("PersonData.normalize_name()")
+        # print("input: {}".format(name))
+        # print("output: {}".format(result))
+        return result
+    def process_name(self, type="pythagorean"):
+        keys = ({"core":["life_path","destiny","hearts_desire","personality"]},
+                {"minor":[]},
+                "pinnacles", "challenges"
+                )
+        self.numbers
+
+    def to_json(self):
+        pass
+    def post_to_db(self):
+        pass
